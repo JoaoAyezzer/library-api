a0bfa0cce37481689b21ebdee8f8bac9f463e19c
JoaoAyezzer <ayezzerp@outlook.com>
2024-11-28 19:13:25 -0300

    Criando estrutura inicial do projeto

---
+++ src/test/java/br/com/elotech/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elotech.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
