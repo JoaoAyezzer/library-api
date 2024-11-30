22c7f9bbea3cb761b80108df23d3e8c6c2d30dde
JoaoAyezzer <ayezzerp@outlook.com>
2024-11-29 23:57:58 -0300

    Documentado a classe JsonFormatConfig para deixar claro o que ela faz

--- src/main/java/br/com/elotech/libraryapi/configs/JsonFormatConfig.java
+++ src/main/java/br/com/elotech/libraryapi/configs/JsonFormatConfig.java
@@ -8,6 +8,25 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Configuração personalizada para serialização de datas e horários no formato JSON.
+ *
+ * <p>
+ * Esta classe define formato padrão para datas e horários no sistema utilizando Jackson.
+ * </p>
+ *
+ * <ul>
+ *   <li>Formato de data: <strong>dd/MM/yyyy</strong></li>
+ *   <li>Formato de data e hora: <strong>dd/MM/yyyy HH:mm:ss</strong></li>
+ * </ul>
+ *
+ * <p>
+ * A configuração é aplicada globalmente em toda a aplicação por meio da anotação {@link Configuration},
+ * que indica que esta classe fornece configurações para o contexto do Spring.
+ * </p>
+ *
+ * @author João Ayezzer
+ */
 @Configuration
 public class JsonFormatConfig {
     private static final String DATE_FORMAT = "dd/MM/yyyy";
