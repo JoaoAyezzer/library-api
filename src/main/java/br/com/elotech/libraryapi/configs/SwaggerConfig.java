90905fb7a81287954bcc40bd100dda4db8df5b0c
JoaoAyezzer <ayezzerp@outlook.com>
2024-11-30 00:06:51 -0300

    Documentado a classe SwaggerConfig para deixar clarro o que ela faz

--- src/main/java/br/com/elotech/libraryapi/configs/SwaggerConfig.java
+++ src/main/java/br/com/elotech/libraryapi/configs/SwaggerConfig.java
@@ -6,6 +6,24 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * Configuração para a documentação utilizando o SpringDoc OpenAPI 3.
+ *
+ * <p>
+ * Esta classe define as configurações necessárias para gerar e personalizar
+ * a documentação da API no formato OpenAPI 3, acessando pelo Swagger UI.
+ * </p>
+ *
+ * <ul>
+ *   <li>Define informações gerais da API, como título e versão.</li>
+ *   <li>Agrupa os endpoints da API por pacotes.</li>
+ * </ul>
+ *
+ * <p>URL padrão da documentação foi personalizada para: <code>/docs</code></p>
+ *
+ * @author João Ayezzer
+ */
 @Configuration
 public class SwaggerConfig {
     @Bean
