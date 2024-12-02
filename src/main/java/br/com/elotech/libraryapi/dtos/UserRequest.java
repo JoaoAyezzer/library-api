8e37cafb9d8756c4a922c7c5a4fd53f025df5d98
JoaoAyezzer <ayezzerp@outlook.com>
2024-12-01 22:20:41 -0300

    Adicionado a anotação para validação de email

--- src/main/java/br/com/elotech/libraryapi/dtos/UserRequest.java
+++ src/main/java/br/com/elotech/libraryapi/dtos/UserRequest.java
@@ -1,10 +1,11 @@
 package br.com.elotech.libraryapi.dtos;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserRequest(
         @NotBlank String name,
-        @NotBlank String email,
+        @Email String email,
         @NotBlank String phone
 ) {
 }
